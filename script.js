f816bb7f5eeab58c46464dc2c7fad7786eb6dc51
yimei-wu <wuyimei60@gmail.com>
2024-06-06 12:00:02 +0200

    fix background and lumio

--- script.js
+++ script.js
@@ -1,26 +1,66 @@
-const lenis = new Lenis({
-  easing: (a) => Math.min(1, 1.001 - Math.pow(2, -10 * a)),
-  orientation: "horizontal",
-  gestureOrientation: "both",
-  syncTouch: true,
-});
+// function init() {
+//   const lenis = new Lenis({
+//     content: document.querySelector(".bg-px-section"),
+//     // infinite: true,
+//     duration: 1,
+//     easing: (a) => Math.min(1, 1.001 - Math.pow(2, -10 * a)),
+//     orientation: "horizontal",
+//     gestureOrientation: "both",
+//   });
 
-function raf(time) {
-  lenis.raf(time);
+//   function raf(time) {
+//     lenis.raf(time);
+//     requestAnimationFrame(raf);
+//   }
+
+//   requestAnimationFrame(raf);
+
+//   lenis.on("scroll", (e) => {
+//     const layers = document.querySelectorAll(".bg-px-layer");
+//     const scrollPosition = window.scrollX;
+
+//     layers.forEach((layer, index) => {
+//       const speedFactor = ((index % 3) + 1) * 0.2;
+//       const offset = scrollPosition * speedFactor;
+//       layer.style.backgroundPosition = `${offset}px center`;
+//     });
+//   });
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   init();
+// });
+
+function init() {
+  const lenis = new Lenis({
+    content: document.querySelector(".bg-px-container"),
+    duration: 1,
+    easing: (a) => Math.min(1, 1.001 - Math.pow(2, -10 * a)),
+    orientation: "horizontal",
+    gestureOrientation: "both",
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
   requestAnimationFrame(raf);
+
+  lenis.on("scroll", (e) => {
+    const layers = document.querySelectorAll(".bg-px-layer");
+    const scrollPercentage = e.progress; // Usa la percentuale di scroll fornita da Lenis
+
+    layers.forEach((layer, index) => {
+      // Calcola un fattore di velocità basato sulla percentuale di scroll
+      const speedFactor = (index + 1) * 0.05; // Aumenta o diminuisce questo valore per regolare la velocità
+      const offset = scrollPercentage * speedFactor;
+      layer.style.backgroundPosition = `${offset}px center`;
+    });
+  });
+  console.log(lenis);
 }
 
-requestAnimationFrame(raf);
-
-lenis.on("scroll", (e) => {
-  const layers = document.querySelectorAll(".bg-px-layer");
-  const scrollPosition = window.scrollX;
-
-  layers.forEach((layer, index) => {
-    const speedFactor = ((index % 3) + 1) * 0.2;
-    const offset = scrollPosition * speedFactor;
-    layer.style.backgroundPosition = `${offset}px center`;
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  init();
 });
-
-window.addEventListener("scroll", function () {});
