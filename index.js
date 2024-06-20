7172f5f14ac881afc76c24bcb77e33f1186a83a4
Yaser Ahmady <y@yaserahmady.com>
2024-06-20 11:28:23 +0200

    Refactor code

---
+++ index.js
@@ -0,0 +1,7 @@
+import "./index.css";
+import gsap from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollToPlugin);
+
+document.addEventListener("DOMContentLoaded", () => {});
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        game: resolve(__dirname, "game.html"),
+      },
+    },
+  },
+});
