b1001bac79be6456e0ae7e33656fa8102ce844d7
Yaser Ahmady <y@yaserahmady.com>
2024-06-22 20:18:44 +0100

    Add loader class

---
+++ js/loader.js
@@ -0,0 +1,40 @@
+import gsap from "gsap";
+
+const DURATION = 3;
+const EASE = "power4.out";
+
+export class Loader {
+  constructor() {
+    this.element = document.getElementById("loader");
+  }
+
+  show(onComplete = null) {
+    gsap.set(this.element, {
+      display: "block",
+      opacity: 0,
+    });
+
+    gsap.to(this.element, {
+      duration: DURATION,
+      opacity: 1,
+      ease: EASE,
+      onComplete: () => {
+        onComplete && onComplete();
+      },
+    });
+  }
+
+  hide(onComplete = null) {
+    gsap.to(this.element, {
+      duration: DURATION,
+      opacity: 0,
+      ease: EASE,
+      onComplete: () => {
+        gsap.set(this.element, {
+          display: "none",
+        });
+        onComplete && onComplete();
+      },
+    });
+  }
+}
